1d759a9e6f423b0f4975aa19036891af99b91b9c
Shahrad Elahi <shahrad@litehex.com>
2025-02-27 13:56:29 +0000

    fix: support for commonjs

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { PrettierConfig } from '@ianvs/prettier-plugin-sort-imports';
-
-declare const DEFAULT_CONFIG: PrettierConfig;
-
-export default DEFAULT_CONFIG;
-
-export type { PrettierConfig };
